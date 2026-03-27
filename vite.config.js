6f266443ae132d598bf29b3b0b063ce4077c2eac
Zoltan <169692067+zoljs@users.noreply.github.com>
2026-03-27 22:42:21 +0100

    fix: added app.css to vite config for laravel cloud deployment

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,11 @@
-import { defineConfig } from 'vite';
-import laravel from 'laravel-vite-plugin';
 import vue from '@vitejs/plugin-vue';
+import laravel from 'laravel-vite-plugin';
+import { defineConfig } from 'vite';
 
 export default defineConfig({
     plugins: [
         laravel({
-            input: 'resources/js/app.ts',
+            input: ['resources/css/app.css', 'resources/js/app.ts'],
             ssr: 'resources/js/ssr.ts',
             refresh: true,
         }),
